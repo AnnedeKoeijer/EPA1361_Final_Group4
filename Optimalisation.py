cd57e2a2e985787af67d25619ff370afb97e36ff
emmavankleef <104896779+emmavankleef@users.noreply.github.com>
2023-06-22 12:57:23 +0200

    Aanpassingen 22/06

--- Optimalisation.py
+++
@@ -1,3 +0,0 @@
-from Sensitivity import setup_dataframe_outcomes
-
-def run_optimalisation(df_outcomes, name):
--- analysis_results.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
-
-from ema_workbench import ema_logging, load_results
-from ema_workbench.analysis import feature_scoring
-
-fn = "./results/run_base.tar.gz"
-experiments, outcomes = load_results(fn)
